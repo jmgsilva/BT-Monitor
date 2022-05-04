fbd95e3b9875b58c5fd8f47391a6f87bd973162a
jmgsilva <jmgsilva22@gmail.com>
2022-05-04 19:51:27 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include <zmq.hpp>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+#include <QApplication>
+
+using std::chrono_literals::operator""s;
+
+int main(int argc, char *argv[])
+{
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
+
+    // construct a REP (reply) socket and bind to interface
+    zmq::socket_t socket{context, zmq::socket_type::rep};
+    socket.bind("tcp://*:5555");
+
+    // prepare some static data for responses
+    const std::string data{"World"};
+
+    for (;;)
+    {
+        std::cout << "Oi" << std::endl;
+        zmq::message_t request;
+
+        // receive a request from client
+        socket.recv(request, zmq::recv_flags::none);
+
+        // simulate work
+        std::this_thread::sleep_for(1s);
+
+        // send the reply to the client
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+    }
+
+    return 0;
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
